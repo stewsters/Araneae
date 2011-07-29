1877fa2ff65922964c1201fc815cd87aeadf16e6
Adrian Moore <adrian.moore@bradleycorp.com>
2011-07-29 09:35:21 -0500

    init

---
+++ src/SpiderMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: akm
+ * Date: 7/29/11
+ * Time: 9:34 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SpiderMain {
+}
